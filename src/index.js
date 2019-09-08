26a0f80a921f3176d8e22379e1a132057224b4b0
rsun <rsun@thoughtworks.com>
2019-09-08 18:04:54 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class Runner {
+  run = () => {
+    console.log('Hello, this is V2EX tool')
+  }
+}
+
+// eslint-disable-next-line
+(function () {
+  // eslint-disable-next-line
+  'use strict';
+  const runner = new Runner();
+  runner.run();
+  // eslint-disable-next-line
+}());
